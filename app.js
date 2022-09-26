b93f35d0454aeed0c329fce1f7852ff8c59a11c1
Federico Nieva <federiconieva@gmail.com>
2022-09-26 19:12:29 +0200

    primer commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const path = require ("path");
+
+const app = express();
+
+app.use(express.static(path.join(__dirname,"/public")));
+
+app.get("/", (req,res) => {
+    res.sendFile (path.join(__dirname, "/views/home.html"));
+    }
+);
+
+app.get("/views/register.html", (req,res) => {
+    res.sendFile (path.join(__dirname, "/views/register.html"));
+    }
+);
+
+app.get("/views/login.html", (req,res) => {
+    res.sendFile (path.join(__dirname, "/views/login.html"));
+    }
+);
+
+
+app.listen(process.env.PORT || 3003, () => console.log("Servidor corriendo"));
+
+
